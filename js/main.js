551724f9de54f77799baa6e528b0471208ddb335
Pietro-Leone <leonepietro1999@gmail.com>
2023-06-30 20:02:31 +0200

    fix e miglioramenti grafici

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Dichiarazione Variabili e Costanti
 const kilometers = parseInt(prompt("Quanti kilometri vuoi percorrere?"));
 const userAge = parseInt(prompt("Quanti anni hai?"));
 const date = new Date();
@@ -8,17 +9,18 @@ const currentDate = ` ${currentDay}/${currentMonth}/${currentYear} `
 
 document.getElementById("date").innerHTML = `${currentDate}`
 
-
+// Calcolo kilometri * prezzo al kilometro
 const kmPrice = kilometers * 0.21;
 
-if (userAge >= 18) {
-    document.getElementById("age").innerHTML = "Adulto"
-    const price = kmPrice;
+// Blocco IF che in base all'età inserita calcola eventuali sconti
+if (userAge >= 65) {
+    const price = kmPrice - (kmPrice * 0.4);
     const priceFix = price.toFixed(2);
     document.getElementById("price").innerHTML = `${priceFix}€`
 }
-else if (userAge >= 65) {
-    const price = kmPrice - (kmPrice * 0.4);
+else if (userAge >= 18) {
+    document.getElementById("age").innerHTML = "Adulto"
+    const price = kmPrice;
     const priceFix = price.toFixed(2);
     document.getElementById("price").innerHTML = `${priceFix}€`
 }
